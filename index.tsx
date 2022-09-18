c0c7eaf4a1ce2552e088d084b484c70b1bfb8ff4
Elliott <forgo@users.noreply.github.com>
2022-09-17 21:00:09 -0600

    rename index.ts to index.tsx

--- index.tsx
+++ index.tsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <html>
       <head>
-        <title>Hello from JSX</title>
+        <title>Elliott</title>
       </head>
       <body>
-        <h1>Hello world</h1>
+        <h1>elliott.software</h1>
       </body>
     </html>
   );
